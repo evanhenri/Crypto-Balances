24650e83fe89339fce4f99d4b2fca24f3b216434
evanhenri <nonce@protonmail.ch>
2015-12-31 19:13:47 -0500

    Address config data variables moved to Family objects

--- src/address.py
+++ src/address.py
@@ -14,15 +14,11 @@ class Family(object):
         self.multi_request_flag = conf[self.family]['MULTI_REQUEST_FLAG_MAX'][0]
         self.multi_request_max = conf[self.family]['MULTI_REQUEST_FLAG_MAX'][1]
         self.multiplier = conf[self.family]['MULTIPLIER']
-        self.single_asset_single_request = not any([self.multi_asset_flag, self.multi_request_flag])
+        self.standard_flag = not any([self.multi_asset_flag, self.multi_request_flag])
     def __call__(self, *args, **kwargs):
         return self.family
-
 
 class Address(object):
     def __init__(self, address, family_obj):
         self.family = family_obj
-        self.address = address
-        self.asset_balances = {} # {'name':balance}
-    def __call__(self, *args, **kwargs):
-        return self.address
+        self.asset_balances = {}
