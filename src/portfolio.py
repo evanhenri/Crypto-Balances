d93b30617114aa1487a9258f49af5b5c2448f11e
evanhenri <nonce@protonmail.ch>
2016-01-02 01:49:06 -0500

    Final refactoring
    Changed structure so that a family object is created for each type of address and contain a list of all addresses for that type. The families are added to a list and looped over so that balances can be retrieved using the appropriate method.

--- src/portfolio.py
+++ src/portfolio.py
@@ -2,131 +2,164 @@ from queue import Queue
 from threading import Thread
 
 from src import util
-from src.address import Address, Family
+
+class Family(object):
+    def __init__(self, family, conf):
+        self.family = family.upper()
+        self.data_key = conf[self.family]['DATA_KEY']
+        self.balance_key = conf[self.family]['BALANCE_KEY']
+        self.api_base = conf[self.family]['API']
+        self.id_key = conf[self.family]['ID_KEY']
+        self.multi_asset_flag = conf[self.family]['MULTI_ASSET_FLAG']
+        self.group_request_flag = conf[self.family]['MULTI_REQUEST_FLAG_MAX'][0]
+        self.multi_request_max = conf[self.family]['MULTI_REQUEST_FLAG_MAX'][1]
+        self.multiplier = conf[self.family]['MULTIPLIER']
+        self.standard_flag = not any([self.multi_asset_flag, self.group_request_flag])
+        self.addresses = []
+    def __call__(self, *args, **kwargs):
+        return self.family
 
 class Portfolio(object):
-    def __init__(self, address_dict, exclusion_lst):
-        self.multi_asset_addresses = {}
-        self.group_request_addresses = {}
-        self.standard_addresses = {}
+    def __init__(self, address_data, address_config, exclusion_lst=[]):
+        self.address_families = []
+        self.address_asset_balances = {}
         self.exclusion_lst = [i.upper() for i in exclusion_lst]
 
-        # Initialize Address by type and add to self.addresses dictionary where k=addr and v=Address
-        for addr_family, addr_lst in address_dict.items():
-            F = Family(addr_family)
+        for addr_type, addr_lst in address_data.items():
+            F = Family(addr_type, address_config)
             for addr in addr_lst:
-                if F.multi_asset_flag:
-                    self.multi_asset_addresses[addr] = Address(addr, F)
-                elif F.multi_request_max:
-                    self.group_request_addresses[addr] = Address(addr, F)
-                elif F.standard_flag:
-                    self.standard_addresses[addr] = Address(addr, F)
+                # initialize empty dict which will be filled with {asset_name:balance,}
+                self.address_asset_balances[addr] = {}
+                # expand address list for each family object to match address config file
+                F.addresses.append(addr)
+            self.address_families.append(F)
 
+        for Fam in self.address_families:
+            if Fam.multi_asset_flag:
+                self.multi_asset_requests(Fam)
+            elif Fam.group_request_flag:
+                self.group_requests(Fam)
+            elif Fam.standard_flag:
+                self.standard_requests(Fam)
+
+    def isempty(self):
+        return len(self.address_families) == 0
+
+    def update_balance(self, address, asset, balance):
+        if asset in self.address_asset_balances[address]:
+            self.address_asset_balances[address][asset] += float(balance)
+        else:
+            self.address_asset_balances[address][asset] = float(balance)
+
+    def multi_asset_requests(self, F):
+        """
+        for addresses that have multiple assets associated with each address e.g. Counterparty
+        """
         q = Queue()
-
-        #multi_asset threaded api requests
         multi_asset_threads = []
-        multi_asset_family = list(self.multi_asset_addresses.values())[0].family
-        for addr in self.multi_asset_addresses.keys():
-            multi_asset_threads.append(Thread(target=util.api_call, args=(multi_asset_family.api_base, addr, q)))
+        for address in F.addresses:
+            multi_asset_threads.append(Thread(target=util.api_call, args=(F.api_base, address, q)))
             multi_asset_threads[-1].start()
         [t.join() for t in multi_asset_threads]
         while not q.empty():
             addr, raw_resp = q.get()
-            resp = util.json_value_by_key(raw_resp, multi_asset_family.data_key)
-
+            resp = util.json_value_by_key(raw_resp, F.data_key)
             for asset_data in resp:
-                asset_type = util.json_value_by_key(asset_data, multi_asset_family.id_key)
-                balance = util.json_value_by_key(asset_data, multi_asset_family.balance_key) * multi_asset_family.multiplier
-                self.update_balance(addr, asset_type, balance, self.multi_asset_addresses)
+                asset_name = util.json_value_by_key(asset_data, F.id_key)
+                asset_balance = util.json_value_by_key(asset_data, F.balance_key) * F.multiplier
+                self.update_balance(addr, asset_name, asset_balance)
 
-        # group_address threaded api request
+    def group_requests(self, F):
+        """
+        for addresses where the specified api allows multiple addresses included in single api call
+        """
+        q = Queue()
         group_request_threads = []
-        group_request_family = list(self.group_request_addresses.values())[0].family
-        max_per_call = group_request_family.multi_request_max
-        addr_lst_chunks = util.chunk_list(list(self.group_request_addresses.keys()), max_per_call)
+        max_per_call = F.multi_request_max
+        addr_lst_chunks = util.chunk_list(list(F.addresses), max_per_call)
 
         for nested_lst in addr_lst_chunks:
             addr_payload = util.merge_lst(nested_lst, ['', ','])
-            group_request_threads.append(Thread(target=util.api_call, args=(group_request_family.api_base, addr_payload, q)))
+            group_request_threads.append(Thread(target=util.api_call, args=(F.api_base, addr_payload, q)))
             group_request_threads[-1].start()
         [t.join() for t in group_request_threads]
         while not q.empty():
             # need to get address from within json reponse to differentiate the balance data,
             # ignore address in position 0 of [address, response] that is returned from q.get()
             raw_resp = q.get()[1]
-            resp_data = util.json_value_by_key(raw_resp, group_request_family.data_key)
+            resp_data = util.json_value_by_key(raw_resp, F.data_key)
             for addr_data in resp_data:
-                addr = util.json_value_by_key(addr_data, group_request_family.id_key)
+                addr = util.json_value_by_key(addr_data, F.id_key)
                 # blockr api sometime sends more responses than were requested as {'':0}, filter them out
                 if addr != '':
-                    balance = util.json_value_by_key(addr_data, group_request_family.balance_key) * group_request_family.multiplier
-                    self.update_balance(addr, self.group_request_addresses[addr].family(), balance, self.group_request_addresses)
+                    asset_name = F()
+                    asset_balance = util.json_value_by_key(addr_data, F.balance_key) * F.multiplier
+                    self.update_balance(addr, asset_name, asset_balance)
 
-        # standard address threaded api request
+    def standard_requests(self, F):
+        """
+        for addresses that have a single asset and whose api has limit of one address per call
+        """
+        q = Queue()
         standard_threads = []
-        standard_family = list(self.standard_addresses.values())[0].family
-        for addr in self.standard_addresses.keys():
-            standard_threads.append(Thread(target=util.api_call, args=(standard_family.api_base, addr, q)))
+        for addr in F.addresses:
+            standard_threads.append(Thread(target=util.api_call, args=(F.api_base, addr, q)))
             standard_threads[-1].start()
         [t.join() for t in standard_threads]
         while not q.empty():
             addr, raw_resp = q.get()
-            resp = util.json_value_by_key(raw_resp, standard_family.data_key)[0]
-            balance = util.json_value_by_key(resp, standard_family.balance_key) * standard_family.multiplier
-            self.update_balance(addr, self.standard_addresses[addr].family(), balance, self.standard_addresses)
-
-    def update_balance(self, addr, asset_name, asset_balance, addr_family_dict):
-        if asset_name in addr_family_dict[addr].asset_balances:
-            addr_family_dict[addr].asset_balances[asset_name] += float(asset_balance)
-        else:
-            addr_family_dict[addr].asset_balances[asset_name] = float(asset_balance)
+            resp = util.json_value_by_key(raw_resp, F.data_key)[0]
+            asset_name = F()
+            asset_balance = util.json_value_by_key(resp, F.balance_key) * F.multiplier
+            self.update_balance(addr, asset_name, asset_balance)
 
     def get_asset_totals(self):
-        addresses = [self.multi_asset_addresses, self.group_request_addresses, self.standard_addresses]
+        """
+        returns dictionary of {asset:balance} with combined totals for all address assets
+        """
         asset_totals = {}
-        for addr_family in addresses:
-            for addr_obj in addr_family.values():
-                for asset, balance in addr_obj.asset_balances.items():
-                    if asset in asset_totals:
-                        asset_totals[asset] += float(balance)
-                    else:
-                        asset_totals[asset] = float(balance)
+        for address, assets in self.address_asset_balances.items():
+            for asset, balance in assets.items():
+                if asset in asset_totals:
+                    asset_totals[asset] += float(balance)
+                else:
+                    asset_totals[asset] = float(balance)
         return asset_totals
 
-    def get_address_totals(self):
-        addresses = [self.multi_asset_addresses, self.group_request_addresses, self.standard_addresses]
-        address_totals = {}
-        for addr_family in addresses:
-            for addr, addr_obj in addr_family.items():
-                for asset, balance in addr_obj.asset_balances.items():
-                    if addr in address_totals:
-                        if asset in address_totals[addr]:
-                            address_totals[addr][asset] += float(balance)
-                        else:
-                            address_totals[addr][asset] = float(balance)
-                    else:
-                        address_totals[addr] = {}
-        return address_totals
-
     def print_address_balances(self):
-        address_totals = self.get_address_totals()
+        """
+        prints individual asset balances for each individual address
+        """
+        for address, assets in self.address_asset_balances.items():
+            # filter out exluded assets and assets with zero balance
+            # format balances to 8 decimal places with commas in thousandths places to measure line lengths
+            filtered_totals = {k:('{0:,.8f}'.format(v)) for k,v in assets.items()
+                               if k not in self.exclusion_lst and v > 0}
+            longest_width = util.longest_kv_length(filtered_totals)
 
-        for addr, addr_assets in address_totals.items():
-            print(addr)
-            for asset, balance in addr_assets.items():
-                #if asset.upper() not in self.exclusion_lst:
-                print('{0}\t{1}'.format(asset, balance))
+            print(address)
+            for asset, balance in filtered_totals.items():
+                line_width = len('{0}{1}'.format(asset, balance))
+                fill = '.' * (longest_width-line_width+5)
+                print('{0}{1}{2}'.format(asset, fill, balance))
             print()
 
     def print_total_balances(self):
+        """
+        prints combined asset totals
+        """
         asset_totals = self.get_asset_totals()
 
-        for asset, balance in asset_totals.items():
-            #if asset.upper() not in self.exclusion_lst:
-            print('{0}\t{1}'.format(asset, balance))
+        # filter out exluded assets and assets with zero balance
+        # format balances to 8 decimal places with commas in thousandths places to measure line lengths
+        filtered_totals = {k:('{0:,.8f}'.format(v)) for k,v in asset_totals.items()
+                           if k not in self.exclusion_lst and v > 0}
+        longest_width = util.longest_kv_length(filtered_totals)
 
-
+        for asset, balance in filtered_totals.items():
+            line_width = len('{0}{1}'.format(asset, balance))
+            fill = '.' * (longest_width-line_width+5)
+            print('{0}{1}{2}'.format(asset, fill, balance))
 
 
 
