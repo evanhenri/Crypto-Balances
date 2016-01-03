c54679719d1f3f2908dfcfd6ad1214d61cd338f0
evanhenri <nonce@protonmail.ch>
2016-01-02 22:17:59 -0500

    Added asset class and filter method for data
    Data structure is now {address:[{asset_name:asset_obj}, ]}. Updated code to work by accessing asset class members for balances

--- src/portfolio.py
+++ src/portfolio.py
@@ -1,7 +1,7 @@
 from queue import Queue
 from threading import Thread
 
-from src import util
+from src import util, exchange
 
 class Family(object):
     def __init__(self, family, conf):
@@ -19,22 +19,29 @@ class Family(object):
     def __call__(self, *args, **kwargs):
         return self.family
 
+class Asset(object):
+    def __init__(self, balance):
+        self.balance = balance
+        self.price = 0
+        self.value = 0
+
 class Portfolio(object):
     def __init__(self, address_data, address_config, exclusion_lst=[]):
-        self.address_families = []
-        self.address_asset_balances = {}
+        self.addr_families = []
+        self.addr_assets = {}
         self.exclusion_lst = [i.upper() for i in exclusion_lst]
+        self.unique_assets = set()
 
         for addr_type, addr_lst in address_data.items():
             F = Family(addr_type, address_config)
             for addr in addr_lst:
-                # initialize empty dict which will be filled with {asset_name:balance,}
-                self.address_asset_balances[addr] = {}
+                # initialize empty dict which will be filled with {asset_name:Asset obj,}
+                self.addr_assets[addr] = {}
                 # expand address list for each family object to match address config file
                 F.addresses.append(addr)
-            self.address_families.append(F)
+            self.addr_families.append(F)
 
-        for Fam in self.address_families:
+        for Fam in self.addr_families:
             if Fam.multi_asset_flag:
                 self.multi_asset_requests(Fam)
             elif Fam.group_request_flag:
@@ -42,14 +49,24 @@ class Portfolio(object):
             elif Fam.standard_flag:
                 self.standard_requests(Fam)
 
-    def isempty(self):
-        return len(self.address_families) == 0
+    def filter_addr_assets(self, min_balance):
+        filtered_addr_assets = {}
+        for address, asset_data in self.addr_assets.items():
+            filtered_addr_assets[address] = {}
+            for asset_name, asset_obj in asset_data.items():
+                if asset_name not in self.exclusion_lst and asset_obj.balance > min_balance:
+                    filtered_addr_assets[address][asset_name] = asset_obj
+        self.addr_assets = filtered_addr_assets
 
-    def update_balance(self, address, asset, balance):
-        if asset in self.address_asset_balances[address]:
-            self.address_asset_balances[address][asset] += float(balance)
+    def isempty(self):
+        return len(self.addr_families) == 0
+
+    def update_balance(self, addr, asset_name, asset_balance):
+        if asset_name in self.addr_assets[addr]:
+            self.addr_assets[addr][asset_name].balance += float(asset_balance)
         else:
-            self.address_asset_balances[address][asset] = float(balance)
+            self.unique_assets.add(asset_name)
+            self.addr_assets[addr][asset_name] = Asset(float(asset_balance))
 
     def multi_asset_requests(self, F):
         """
@@ -57,8 +74,8 @@ class Portfolio(object):
         """
         q = Queue()
         multi_asset_threads = []
-        for address in F.addresses:
-            multi_asset_threads.append(Thread(target=util.api_call, args=(F.api_base, address, q)))
+        for addr in F.addresses:
+            multi_asset_threads.append(Thread(target=util.api_call, args=(F.api_base, addr, q)))
             multi_asset_threads[-1].start()
         [t.join() for t in multi_asset_threads]
         while not q.empty():
@@ -118,30 +135,35 @@ class Portfolio(object):
         returns dictionary of {asset:balance} with combined totals for all address assets
         """
         asset_totals = {}
-        for address, assets in self.address_asset_balances.items():
-            for asset, balance in assets.items():
-                if asset in asset_totals:
-                    asset_totals[asset] += float(balance)
+        for asset_data in self.addr_assets.values():
+            for asset_name, asset_obj in asset_data.items():
+                if asset_name in asset_totals:
+                    asset_totals[asset_name] += float(asset_obj.balance)
                 else:
-                    asset_totals[asset] = float(balance)
+                    asset_totals[asset_name] = float(asset_obj.balance)
         return asset_totals
 
-    def print_address_balances(self):
+    def print_address_balances(self, precision=8):
         """
         prints individual asset balances for each individual address
         """
-        for address, assets in self.address_asset_balances.items():
-            # filter out exluded assets and assets with zero balance
-            # format balances to 8 decimal places with commas in thousandths places to measure line lengths
-            filtered_totals = {k:('{0:,.8f}'.format(v)) for k,v in assets.items()
-                               if k not in self.exclusion_lst and v > 0}
-            longest_width = util.longest_kv_length(filtered_totals)
+        # do not print addresses with no assets to show
+        formatted = {}
+        longest_width = 0
+        for address, asset_data in self.addr_assets.items():
+            formatted[address] = {}
+            for asset_name, asset_obj in asset_data.items():
+                asset_obj.balance = '{0:,.{p}f}'.format(asset_obj.balance, p=precision)
+                width = len('{0}{1}'.format(asset_name, asset_obj.balance))
+                if width > longest_width: longest_width = width
+                formatted[address][asset_name] = asset_obj
 
+        for address, asset_data in formatted.items():
             print(address)
-            for asset, balance in filtered_totals.items():
-                line_width = len('{0}{1}'.format(asset, balance))
+            for asset_name, asset_obj in asset_data.items():
+                line_width = len('{0}{1}'.format(asset_name, asset_obj.balance))
                 fill = '.' * (longest_width-line_width+5)
-                print('{0}{1}{2}'.format(asset, fill, balance))
+                print('{0}{1}{2}'.format(asset_name, fill, asset_obj.balance))
             print()
 
     def print_total_balances(self):
@@ -160,6 +182,7 @@ class Portfolio(object):
             line_width = len('{0}{1}'.format(asset, balance))
             fill = '.' * (longest_width-line_width+5)
             print('{0}{1}{2}'.format(asset, fill, balance))
+
 
 
 
