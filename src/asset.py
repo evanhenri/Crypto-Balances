05c3d2b53af283d3928a27b68306c36203d95208
evanhenri <nonce@protonmail.ch>
2016-01-03 14:35:38 -0500

    Prices.get support for both cryptocurrency and fiat

--- src/asset.py
+++ src/asset.py
@@ -1,4 +1,10 @@
 from src import util
+from queue import Queue
+from threading import Thread
+
+import decimal
+from decimal import Decimal
+decimal.getcontext().prec = 8
 
 class Asset(object):
     def __init__(self, balance):
@@ -6,31 +12,54 @@ class Asset(object):
         self.price = 0
         self.value = 0
 
-class Price(object):
-    def __init__(self, api_base, ticker_method):
-        self.api_base = api_base
-        self.raw_ticker_data = util.api_call(api_base, ticker_method)
-        self.ticker_data_by_symbol = {}
-        self.ticker_data_by_name = {}
-        for currency in self.raw_ticker_data:
-            ticker_symbol = currency['id'].strip().upper()
-            ticker_name = currency['name'].strip().upper()
-            self.ticker_data_by_symbol[ticker_symbol] = float(currency['price_btc'])
-            self.ticker_data_by_name[ticker_name] = float(currency['price_btc'])
+class Prices(object):
+    def __init__(self):
+        self.cryptocurrency_ticker_symbols = {}
+        self.cryptocurrency_ticker_names = {}
+        self.fiat_ticker_symbols = {}
 
-    def get_price(self, target_currency, base_currency):
-        print(target_currency)
-        target = target_currency.upper()
-        base = base_currency.upper()
-        if base == 'BTC':
-            try:
-                return self.ticker_data_by_symbol[target]
-            except KeyError:
-                try:
-                    return self.ticker_data_by_name[target]
-                except KeyError:
-                    for name, price in self.ticker_data_by_name.items():
-                        if target in name:
-                            return price
-            return 0
+        q = Queue()
+        threads = [Thread(target=util.api_call, args=('http://api.cryptocoincharts.info/', 'listCoins', q)),
+                   Thread(target=util.api_call, args=('http://api.bitcoincharts.com/v1/', 'markets.json', q))]
+        [t.start() for t in threads]
+        [t.join() for t in threads]
+
+        while not q.empty():
+            url_path, ticker_data = q.get()
+            if url_path == 'listCoins':
+                for ticker in ticker_data:
+                    btc_price = Decimal(ticker['price_btc'])
+                    if btc_price > 0:
+                        self.cryptocurrency_ticker_symbols[ticker['id'].upper()] = btc_price
+                        self.cryptocurrency_ticker_names[ticker['name'].upper()] = btc_price
+            elif url_path == 'markets.json':
+                for ticker in ticker_data:
+                    btc_price = ticker['bid']
+                    if btc_price:
+                        self.fiat_ticker_symbols[ticker['currency']] = Decimal(btc_price)
+
+    def get(self, target_currency, base_currency):
+        def currency_to_btc(currency):
+            if currency in self.cryptocurrency_ticker_symbols:
+                return self.cryptocurrency_ticker_symbols[currency]
+            elif currency in self.fiat_ticker_symbols:
+                return self.fiat_ticker_symbols[currency]
+            elif currency in self.cryptocurrency_ticker_names:
+                return self.cryptocurrency_ticker_names[currency]
+            else:
+                return Decimal(1)
+
+        target, base = target_currency.upper(), base_currency.upper()
+        target_price = currency_to_btc(target)
+        base_price = currency_to_btc(base)
+
+        # test if Decimal(0) is False
+        if all([target_price, base_price]):
+            if base != 'BTC':
+                if base in self.cryptocurrency_ticker_symbols or base in self.cryptocurrency_ticker_names:
+                    target_price /= base_price
+                else:
+                    target_price *= base_price
+            return target_price
+        return 0
 
